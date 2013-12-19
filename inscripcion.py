b1934b36aea4716cf9f217772fad78f1106da645
David <dvdalilue@gmail.com>
2013-12-19 14:49:18 -0430

    Cambios el asistencia y .gitignore

--- inscripcion.py
+++ inscripcion.py
@@ -1,1 +1,15 @@
-creando inscripcion
+import 
+
+class asistente(object):
+
+    def __init__(self, cod, tel, url, asist, desc):
+
+        self.asistencia = asist
+        self.cod_postal = cod
+        self.telefono = tel
+        self.url = url
+        self.desc = desc
+
+    def calcularPrecioInsc(self, monto):
+
+        return self.desc*monto
