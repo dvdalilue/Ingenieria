ab76ca42703c7d08eaf0f0d2b178e8eb4a32ba65
Audry Morillo <audrymorillo@gmail.com>
2013-12-20 00:55:15 -0430

    Archivos con identacion minusculas

--- Coalicion/Articulo.py
+++ Coalicion/Articulo.py
@@ -9,25 +9,28 @@
 # Seccion : 1
 #
 #*****************************************************************************
-from Persona import Persona
+from Persona import *
 
 class Articulo:
 
-    def __init__(self, Id, Titulo, Autor, Palabras_Claves, Topico, Texto, Resumen):
-        """Constructor"""
-        self.Id_Articulo = Id
-        self.Titulo = Titulo
-        self.Autor = Autor
-        self.Palabras_Claves = Palabras_Claves
-        self.Texto = self.Texto
-        self.Resumen = self.Resumen
-        self.Topico = Topico
-        self.Jurado = []                # Arreglo de miembro_cp que evaluan el articulo
-        self.Aceptable = False            # es true si el puntaje promedio del articulo es mayot o igual a 3,00
-        self.Calificaciones = []            # almacena el voto de cada miembro de cp
-        self.Puntaje_promedio = 0
+    def __init__(self, id, titulo, autor, palabras_claves, 
+                 topico, texto, resumen):
         
-    def Calificar(self, miembro_cp, puntuacion):
+        """Constructor"""
+        self.id_articulo = id
+        self.titulo = titulo
+        self.autor = autor
+        self.palabras_claves = palabras_claves
+        self.texto = self.texto
+        self.resumen = self.resumen
+        self.topico = topico
+        self.jurado = []         # Arreglo de miembro_cp que evaluan el articulo
+        self.aceptable = False   
+        # es true si el puntaje promedio del articulo es mayor o igual a 3,00
+        self.calificaciones = []   # almacena el voto de cada miembro de cp
+        self.puntaje_promedio = 0
+        
+    def calificar(self, miembro_cp, puntuacion):
         """ Metodo : Calificar
        Parametros : self 
              Miembro_Cp  miembro_cp, 
@@ -36,19 +39,22 @@ class Articulo:
                       esa calificacion al articulo
       """
     # se verifica que un miembro no vote mas de una vez
-        if miembro_cp in self.Jurado:
+        if miembro_cp in self.jurado:
             print 'ya voto'
             return False
         
-            #si la puntuacion es valida se alamacena en el arreglo de Calificaciones
-            # y se registra el miembro que voto por el articulo en el arreglo Jurado
+            # si la puntuacion es valida se alamacena en el 
+            # arreglo de Calificaciones
+            # y se registra el miembro que voto por el articulo 
+            # en el arreglo Jurado
         if 0 < puntuacion <= 5:
             # and puntuacion <= 5:
-            self.Calificaciones.append(puntuacion)
-            self.Jurado.append(miembro_cp) 
+            self.calificaciones.append(puntuacion)
+            self.jurado.append(miembro_cp) 
             return True
             
-        print('La calificacion no tuvo exito : puntaje invalido, debe ingresar un numero del 1 al 5,')
+        print('La calificacion no tuvo exito : puntaje invalido,'+ 
+                                'debe ingresar un numero del 1 al 5,')
         return False
               
     def calcular_promedio(self):
@@ -57,24 +63,25 @@ class Articulo:
        Descripcion; toma el arreglo de Calificaciones y suma cada uno de los
                        votos dividiendolo por el numero de votantes
       """
-        self.Puntaje_promedio = 0
-        for n in self.Calificaciones:
-            self.Puntaje_promedio += n
+        self.puntaje_promedio = 0
+        for n in self.calificaciones:
+            self.puntaje_promedio += n
 
-        if self.Puntaje_promedio > 0:
-            self.Puntaje_promedio = (self.Puntaje_promedio)/(float(len(self.Calificaciones)))
+        if self.puntaje_promedio > 0:
+            self.puntaje_promedio = (self.puntaje_promedio)/(float(len(self.calificaciones)))
 
-    def verificar_Aceptable(self):
+    def verificar_aceptable(self):
         """ Metodo : verificar_Aceptable
         Parametros : self
         Descripcion; calcula el puntaje promedio de un articulo, si es mayor
-        o igual a 3,00 coloca el atributo aceptable en True, sino permanece False
+        o igual a 3,00 coloca el atributo aceptable en True, 
+        sino permanece False
         """
-        if len(self.Jurado) > 2:
+        if len(self.jurado) > 2:
             self.calcular_promedio()
-            if self.Puntaje_promedio >= float(3):
-                self.Aceptable = True
-        return self.Aceptable
+            if self.puntaje_promedio >= float(3):
+                self.aceptable = True
+        return self.aceptable
         
             
     def __str__(self):
@@ -85,7 +92,7 @@ class Articulo:
         keys = self.__dict__.keys()
         datos = ""
         for n in keys:
-            if n != 'Calificaciones' and n != 'Id_Articulo' and n != 'Jurado':
+            if n != 'Calificaciones' and n != 'id_Articulo' and n != 'Jurado':
                 datos += "\n%s: %s"%(n,str(self.__dict__[n]))
         datos += "\n"
         return datos
