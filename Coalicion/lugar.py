b4188c10a29d8cad4c982239f668a954e835f0de
VaneRivas <vanessa_rivas_18@hotmail.com>
2013-12-19 21:12:50 -0430

    Modificando clase lugar

--- Coalicion/lugar.py
+++ Coalicion/lugar.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python
 
 class Lugar(object):
-    def __init__(self, influgares=None, lugares = None):
-        if influgares is None:
-            influgares = []
-            lugares = []
-        self.influgares = influgares
-        self.lugares = lugares
-
-    def add(self,nombre,ubicacion, capacidad):
-        self.influgares.append([nombre,ubicacion, capacidad])
-        self.lugares.append(nombre)
-
+    def __init__(self, Nombre, Ubicacion, Capacidad_maxima):
+       
+       self.Nombre = Nombre
+       self.Ubicacion = Ubicacion
+       self.Capacidad_maxima = Capacidad_maxima
+       
+    def Equals(self, Lugar):
+        if self.Nombre == Lugar.Nombre:
+	   print "Ambos Lugares tIenen el mismo Nombre"
+	   if self.Ubicacion == Lugar.Ubicacion:
+	    return True
+	else:
+	    return False
+            
     def __str__(self):
-        return str(self.influgares)
+        keys = self.__dict__.keys()
+        datos = ""
+        for n in keys:
+            datos += "\n%s: %s"%(n,str(self.__dict__[n]))
+        datos += "\n"
+        return datos
 
-    def buscar(self, valor):
-        if valor in self.lugares:
-            print("el lugar esta creado")
-        else:
-            print("el lugar no existe")		
+ if __name__=="__main__":
+    
+    l = Lugar("MYS", "Mys-019", 300)
+    print l
