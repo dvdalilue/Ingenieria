4b606be06838994a0cd4e85fcda281892a7e9bf9
mwoo <maik302@gmail.com>
2014-01-28 15:02:38 -0430

    Modificaciones al modulo de Articulos.
    - Maik

---
+++ CLEI/articulos/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from articulos import fields
+
+class Articulo(models.Model):
+  titulo = models.CharField(max_length=100)
+  palabras_clave = models.ManyToManyField('PalabraClave')
+  topico = models.ManyToManyField('Topico')
+  # Es necesario tener el texto del articulo?
+  #texto = models.TextField()
+  resumen = models.TextField()
+  aceptable = models.BooleanField(default=False)
+  puntaje_promedio = models.FloatField()
+  #calificaciones = CAMPO NUEVO
+
+class PalabraClave(models.Model):
+  palabra = models.CharField(max_length=25)
+
+class Topico(models.Model):
+  topico = models.CharField(max_length=25)
