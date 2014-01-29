2889ea76f640288291f9a6b40deabf1355cb6c5b
David <dvdalilue@gmail.com>
2014-01-29 14:37:42 -0430

    Agarra lo que se habia hecho en vero

--- CLEI/evento/models.py
+++ CLEI/evento/models.py
@@ -2,12 +2,11 @@ from django.db   import models
 from clei.models import Topico
 
 class Lugar(models.Model):
-
     nombre           = models.CharField(max_length=60)
     ubicacion        = models.CharField(max_length=200)
     capacidad_maxima = models.IntegerField(default=0)
-   # ...
-    def __unicode__(self):  # Python 3: def __str__(self):
+
+    def __unicode__(self):
         return self.nombre
 
 class Evento(models.Model):
--- Django_Vero/aprendiendo_django/CLEI/views.py
+++
@@ -1,5 +0,0 @@
-#escribir para el view..
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
